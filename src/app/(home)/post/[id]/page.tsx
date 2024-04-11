da45e05d28549a815b72a90e2a1beee3e38179f2
trieu1212 <lequoctrieu618@gmail.com>
2024-04-11 17:57:08 +0700

    fix folder

--- src/app/(home)/post/[id]/page.tsx
+++ src/app/(home)/post/[id]/page.tsx
@@ -4,6 +4,7 @@ import OnePost from "@/app/(home)/post/[id]/one-post";
 export default function Page({ params }: { params: { id: string } }) {
     return (
       <>
+      <h1>đây là bài viết</h1>
         <OnePost id={params.id} />
       </>
     )
